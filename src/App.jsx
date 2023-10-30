10f7499202c5e7efaa472dccd24b164212a86619
abir2010 <kaiserabir222@gmail.com>
2023-10-31 02:11:26 +0600

    navigation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='text-3xl font-bold underline'>
-      hello world
+    <div className="">
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
